d7936d9beda143eb0b0ed09978b693a9cbb64a41
AlanIbarra117 <148792188+AlanIbarra117@users.noreply.github.com>
2025-02-01 03:04:17 +0000

    lab 03 completed

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,21 @@
+public class BankAccount {
 
+    public static void main(String[] args) {
+
+        // Initial balance
+        double balance = 2175.37;
+
+        // Bob's transactions
+        balance -= 302.50;      // Bob withdrew $302.50
+        balance += 50.03;       // Bob deposited $50.03
+        balance -= balance / 2; // Bob withdrew half of his current balance
+        balance += 20.00;       // Bob deposited $20.00
+        balance -= 1.00;        // Bob withdrew $1
+        balance *= 2;           // Bob deposited his paycheck, doubling his balance
+        balance += 1.00;      // Bob deposited $1
+
+        // Output the final balance
+        System.out.printf("Bob's new balance: $%.2f%n", balance);
+
+    }
+}
